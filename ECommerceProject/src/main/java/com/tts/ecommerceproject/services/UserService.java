8224f730cec0c3fcd77e626dcfdcfb5640a7328f
Carissa Hibbert <108243658+carhibbert@users.noreply.github.com>
2022-11-05 15:24:55 -0500

    Add files via upload

---
+++ ECommerceProject/src/main/java/com/tts/ecommerceproject/services/UserService.java
@@ -0,0 +1,58 @@
+package com.tts.ecommerceproject.services;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.tts.ecommerceproject.models.Product;
+import com.tts.ecommerceproject.models.User;
+import com.tts.ecommerceproject.repositories.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public User findByUsername(String username) {
+		return (User) userRepository.findByUsername(username);
+	}
+	
+	public void saveNew(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		userRepository.save(user);
+	}
+	
+	public void saveExisting(User user) {
+		userRepository.save(user);
+	}
+	public User getLoggedInUser() {
+		return
+findByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+	}
+	
+	public void updateCart(Map<Product, Integer> cart) {
+		User user = getLoggedInUser();
+		user.setCart(cart);
+		saveExisting(user);
+	}   
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = findByUsername(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("Username not found.");
+		}
+		return user;
+	}
+	
+}
